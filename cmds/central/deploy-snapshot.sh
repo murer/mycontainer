d627d5649f4aaf5613623dccbeb7224efbe3bbb9
murer <murer@dextra-sw.com>
2014-07-02 18:30:20 -0300

    x

--- cmds/central/deploy-snapshot.sh
+++ cmds/central/deploy-snapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-mvn clean verify source:jar-no-fork javadoc:jar gpg:sign deploy:deploy "-Dgpg.passphrase=$MYCONTAINER_SECRET" -DdeployCentral -DaltDeploymentRepository=sonatype-nexus-snapshots::default::https://oss.sonatype.org/content/repositories/snapshots
+mvn clean verify source:jar-no-fork javadoc:jar gpg:sign deploy:deploy "-Dgpg.passphrase=$MYCONTAINER_SECRET" -DdeployCentral -DaltDeploymentRepository=sonatype-nexus-snapshots::default::https://oss.sonatype.org/content/repositories/snapshots -Dmaven.test.skip.exec
 
