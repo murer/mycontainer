385198c94672b57227304d1c980abf8c2e0f4311
murer <murer@dextra-sw.com>
2014-07-03 08:57:10 -0300

    x

--- cmds/central/deploy-release.sh
+++ cmds/central/deploy-release.sh
@@ -5,5 +5,5 @@ if grep version pom.xml | grep SNAPSHOT; then
 	exit 1;
 fi
 
-mvn clean verify source:jar-no-fork javadoc:jar gpg:sign deploy:deploy "-Dgpg.passphrase=$MYCONTAINER_SECRET" -DdeployCentral -DaltDeploymentRepository=sonatype-nexus-staging::default::https://oss.sonatype.org/service/local/staging/deploy/maven2
+mvn clean verify source:jar-no-fork javadoc:jar gpg:sign deploy:deploy "-Dgpg.passphrase=$MYCONTAINER_SECRET" -DdeployCentral -DaltDeploymentRepository=sonatype-nexus-staging::default::https://oss.sonatype.org/service/local/staging/deploy/maven2 -Dmaven.test.skip.exec
 
