0b6369fdef2d122c4aebd5c403ee5cca8ffe040a
Filipe Carmona Simões <filipe.csimoes@gmail.com>
2015-02-09 13:58:26 -0200

    LookupUtil

--- mycontainer-components/mycontainer-ejb/src/main/java/com/googlecode/mycontainer/ejb/LookupUtil.java
+++ mycontainer-components/mycontainer-ejb/src/main/java/com/googlecode/mycontainer/ejb/LookupUtil.java
@@ -11,9 +11,9 @@ public class LookupUtil {
 
 	public static Object lookupEJB(EJB ejb, Class<?> type, Context context) throws NamingException {
 		String beanName;
-		if (ejb.mappedName().trim().length() > 0) {
+		if (ejb != null && ejb.mappedName().trim().length() > 0) {
 			beanName = ejb.mappedName().trim();
-		} else if (ejb.beanInterface() == Object.class) {
+		} else if (ejb != null && ejb.beanInterface() == Object.class) {
 			beanName = MyNameParser.parseClassName("ejb", type);
 		} else {
 			beanName = MyNameParser.parseClassName("ejb", ejb.beanInterface());
@@ -23,9 +23,9 @@ public class LookupUtil {
 
 	public static Object lookupResource(Resource resource, Class<?> type, Context context) throws NamingException {
 		String beanName;
-		if (resource.mappedName().trim().length() > 0) {
+		if (resource != null && resource.mappedName().trim().length() > 0) {
 			beanName = resource.mappedName().trim();
-		} else if (resource.type() == Object.class) {
+		} else if (resource != null && resource.type() == Object.class) {
 			beanName = MyNameParser.parseClassName("resource", type);
 		} else {
 			beanName = MyNameParser.parseClassName("resource", resource.type());
