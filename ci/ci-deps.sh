dd4b6e674923212739e736d382660574d409fbb0
murer <murer@dextra-sw.com>
2017-03-25 22:17:36 -0300

    release

--- ci/ci-deps.sh
+++ ci/ci-deps.sh
@@ -6,3 +6,15 @@ mvn --version
 #./cmds/decrypt.sh
 
 #gpg --import keys/pyrata.org.priv
+
+if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
+  if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
+  mvn clean -B -Dmaven.test.skip.exec -T 10;
+  fi;
+fi
+
+if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
+  if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
+  mvn install -B -Dmaven.test.skip.exec -T 10;
+  fi;
+fi
