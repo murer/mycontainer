847ffa433fa4ecc14f4ab6a4e60f97e9d570acad
murer <murer@dextra-sw.com>
2017-03-26 10:55:55 -0300

    release

--- cmds/decrypt.sh
+++ cmds/decrypt.sh
@@ -1,12 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 if [ "x$MYCONTAINER_SECRET" == "x" ]; then
 	echo "export MYCONTAINER_SECRET to descrypt files";
 	exit 1;
 fi
 
-find -name "*.crypt" | while read k; do 
+find -name "*.crypt" | while read k; do
 	echo "decrypt: $k";
-	openssl enc -aes-256-cbc -salt -in "$k" -out "$(echo "$k" | sed "s/\.crypt$//g")" -d -pass "pass:$REPOZSECRET"; 
+	openssl enc -aes-256-cbc -salt -in "$k" -out "$(echo "$k" | sed "s/\.crypt$//g")" -d -pass "pass:$REPOZSECRET";
 done
-
