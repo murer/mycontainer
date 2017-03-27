bbec11db6177974dd51052769d43541d466252b3
murer <murer@dextra-sw.com>
2017-03-27 07:24:01 -0300

    release

--- ci/ci-relase.sh
+++ ci/ci-relase.sh
@@ -1,20 +1,5 @@
 #!/bin/bash -xe
 
-java -version
-mvn --version
-
-#./cmds/decrypt.sh
+./cmds/decrypt.sh
 
 #gpg --import keys/pyrata.org.priv
-
-if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
-  if ! mvn clean -B -Dmaven.test.skip.exec -T 10; then
-  mvn clean -B -Dmaven.test.skip.exec -T 10;
-  fi;
-fi
-
-if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
-  if ! mvn install -B -Dmaven.test.skip.exec -T 10; then
-  mvn install -B -Dmaven.test.skip.exec -T 10;
-  fi;
-fi
--- cmds/encrypt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-if [ "x$MYCONTAINER_SECRET" == "x" ]; then
-        echo "export MYCONTAINER_SECRET to descrypt files";
-        exit 1;
-fi
-
-openssl enc -aes-256-cbc -salt -in "$1" -out "$1.crypted" -pass "pass:$MYCONTAINER_SECRET";
