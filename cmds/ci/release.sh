944daaae98e0e46ccfa6110f203838398acd0fdd
murer <murer@dextra-sw.com>
2017-03-27 16:47:00 -0300

    fix

--- cmds/ci/release.sh
+++ cmds/ci/release.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -xe 
+#!/bin/bash -xe
 
 ./cmds/ci/script.sh
 cp cmds/ci/settings.priv $HOME/.m2/settings.xml
-./cmds/central/deploy-release.sh 
-rm $HOME/.m2/settings.xml
 
+# we have moved to circleci
+#./cmds/central/deploy-release.sh
+
+rm $HOME/.m2/settings.xml
