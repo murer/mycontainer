c215c9f62cd839139855bab44985b2af7ad22550
murer <murer@dextra-sw.com>
2017-03-27 16:45:48 -0300

    fix

--- cmds/ci/snapshot.sh
+++ cmds/ci/snapshot.sh
@@ -2,6 +2,8 @@
 
 ./cmds/ci/script.sh
 cp cmds/ci/settings.priv $HOME/.m2/settings.xml
-./cmds/central/deploy-snapshot.sh 
-rm $HOME/.m2/settings.xml
 
+# we have moved to circleci
+#./cmds/central/deploy-snapshot.sh
+
+rm $HOME/.m2/settings.xml
