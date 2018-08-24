976bd7900aad6b20307a8dfdc6ca4a75a593041e
murer <murer@dextra-sw.com>
2018-08-24 13:44:06 -0300

    x

--- mvn_ops.sh
+++ mvn_ops.sh
@@ -1,3 +1,3 @@
 
-export MAVEN_OPTS="-Xms512m -Xmx512m -XX:PermSize=256m -XX:MaxPermSize=256m"
-
+#export MAVEN_OPTS="-Xms512m -Xmx512m -XX:PermSize=256m -XX:MaxPermSize=256m"
+export MAVEN_OPTS="-Xms1024m -Xmx1024m"
